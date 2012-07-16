3efe0c96c7fb65060e8e76fbcee59837864dba46
Wajdy Essam <wajdyessam@ksu.edu.sa>
2012-07-17 01:38:07 +0300

    remove unused comments

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -9,26 +9,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-//    // get all categories
-//    Mango::DatabaseManager databaseManager;
-//    std::vector<Mango::Category> categories = databaseManager.getCategories();
-//    for(std::vector<Mango::Category>::iterator p = categories.begin();
-//            p != categories.end(); ++p ) {
-//        qDebug() << p->getId() << " : " << QString::fromStdString(p->getEnglishName());
-//    }
-
-//    // get all items in categories
-//    int categoryId = 3;
-//    std::vector<Mango::Item> items = databaseManager.getItemsInCategory(categoryId);
-//    for(std::vector<Mango::Item>::iterator p = items.begin(); p != items.end(); ++p ) {
-//        qDebug() << p->getId() << " : " << QString::fromStdString(p->getEnglishName()) << " " << p->getPrice();
-//    }
-
-//    // get item information
-//    Mango::Item item = databaseManager.getItemDetails(15);
-//    qDebug() << endl << item.getId() << " " << QString::fromStdString(item.getEnglishName()) << " " << item.getPrice();
-
     this->setWindowSize();
     this->addWidgets();
     this->addButtons();
@@ -86,50 +66,6 @@ void MainWindow::addButtons() {
     this->headerWidget = new QWidget;
     headerDockWidget->setWidget(this->headerWidget);
     this->addDockWidget(Qt::LeftDockWidgetArea, headerDockWidget);
-
-//    // buttons size
-//    int x = 19;
-//    int y = 160 ;
-//    int height = 72 ;
-//    int width = 175;
-//    int spaceBetweenButtons = 5 ;
-
-//    QPushButton *welcomeButton = new QPushButton(this);
-//    welcomeButton->setText("Registeration");
-//    welcomeButton->setCheckable(true);
-//    welcomeButton->setChecked(true);
-//    welcomeButton->setGeometry(x,y,width,height);
-//    welcomeButton->setObjectName("0_toolButton");
-//    welcomeButton->setToolTip("Main Application");
-//    welcomeButton->setStatusTip("QuickAuth");
-//    connect(welcomeButton, SIGNAL(clicked()), signalMapper, SLOT(map()));
-//    this->signalMapper->setMapping(welcomeButton, Welcome);
-
-//    y += height + spaceBetweenButtons;
-
-//    QPushButton *manageUsersButton = new QPushButton(this);
-//    manageUsersButton->setText("Manage Users");
-//    manageUsersButton->setCheckable(true);‏‪(11:06:00 PM)‬‏‬ ‪suda.nix2010‬‏‬: ‫وتستاهلي كل خير
-//    manageUsersButton->setGeometry(x,y,width,height);
-//    manageUsersButton->setObjectName("2_toolButton");
-//    manageUsersButton->setToolTip("Manage Users");
-//    manageUsersButton->setStatusTip("View and Delete Users");
-//    connect(manageUsersButton, SIGNAL(clicked()), signalMapper, SLOT(map()));
-//    this->signalMapper->setMapping(manageUsersButton, Manage_User);
-
-//    y += height + spaceBetweenButtons;
-
-//    QPushButton *manageServiceButton = new QPushButton(this);
-//    manageServiceButton->setText("Manage Service");
-//    manageServiceButton->setGeometry(x,y,width,height);
-//    manageServiceButton->setCheckable(true);
-//    manageServiceButton->setObjectName("3_toolButton");
-//    manageServiceButton->setToolTip("Manage the Camera");
-//    manageServiceButton->setStatusTip("Start or Release the Camera");
-//    connect(manageServiceButton, SIGNAL(clicked()), signalMapper, SLOT(map()));
-//    this->signalMapper->setMapping(manageServiceButton, Manage_Service);
-
-//    connect(this->signalMapper, SIGNAL(mapped(int)), this, SLOT(setCurrentWindow(int)));
 }
 
 void MainWindow::addStatusBar() {
