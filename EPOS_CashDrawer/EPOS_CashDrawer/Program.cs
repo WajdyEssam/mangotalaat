152a6246931e85866a9563c634fc2bc2eb7fde6c
Ahmed Isam <suda.nix@hotmail.com>
2013-03-14 13:07:26 +0300

    Add Support for opening EPOS Cash Drawer

---
+++ EPOS_CashDrawer/EPOS_CashDrawer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace EPOS_CashDrawer
+{
+    public class PortAccess
+    {
+        [DllImport("CashDrawer.dll", EntryPoint = "Initial_CashDrawer")]
+        public static extern void Initial_CashDrawer();
+        [DllImport("CashDrawer.dll", EntryPoint = "fnPxGetCashDrawerStatus")]
+        public static extern bool fnPxGetCashDrawerStatus(short num_drawer);
+        [DllImport("CashDrawer.dll", EntryPoint = "fnPxCashDrawerOpen")]
+        public static extern bool fnPxCashDrawerOpen(short num_drawer);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PortAccess.Initial_CashDrawer();
+            PortAccess.fnPxCashDrawerOpen(0x01);
+        }
+    }
+}
